381e129af287155dfd2eb6adb610cf221c1768bf
Cao Mai <caomai@rocketmail.com>
2021-01-26 21:42:55 -0800

    Add Privacy Policy

--- next-chord/Controller/HomeVC.swift
+++ next-chord/Controller/HomeVC.swift
@@ -104,6 +104,7 @@ class HomeVC: UIViewController {
 
 
 extension HomeVC: UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         if collectionView == minorChordCV {
             return allMinorKeys.count
